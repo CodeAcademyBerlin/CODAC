aea2b39b4f82af7936e382f9a24e411d443a17fe
Lucas Dupias <lucas@codeacademyberlin.com>
2023-03-24 16:38:27 +0100

    fix: :ambulance: restructure

--- apps/codac-ts-next-mui/.lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx,md,mdx}': (filenames) =>
-    `next lint --fix --file ${filenames
-      .map((file) => file.split(process.cwd()[1]))
-      .join(' --file ')}`,
-  '*': 'prettier --ignore-unknown --config .prettierrc --write',
-};
--- apps/codac-ts-next-mui/src/hooks/useSocket.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react';
-
-import { SocketContext, SocketContextIface } from '../contexts/socketContext';
-
-export const useSocket = (): SocketContextIface => useContext(SocketContext);
