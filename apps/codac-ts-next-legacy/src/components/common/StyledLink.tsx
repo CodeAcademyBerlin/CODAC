ba108b7d6cf313816f4654f803345fdb51718523
Lucas Dupias <lucas@codeacademyberlin.com>
2023-03-24 12:00:40 +0100

    fix: :ambulance: restructure merge

--- apps/codac-ts-next-legacy/src/components/common/StyledLink.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material/styles';
-import Link from 'next/link';
-
-const StyledLink = styled(Link)({
-  display: 'flex',
-  alignItems: 'center',
-  textDecoration: 'none',
-});
-export default StyledLink;
