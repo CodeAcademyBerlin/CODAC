3d341d11835d50cd1ebcf5ca01077deb361ae291
santiagomanso <santiagomanso@web.de>
2023-03-30 14:34:58 +0200

    🔧 added border default

--- apps/react-tailwind-components/src/components/buttons/Button.tsx
+++ apps/react-tailwind-components/src/components/buttons/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps } from '../interfaces/interfaceButtons'
+import { ButtonProps } from '../../interfaces/interfaceButtons'
 
 const Button = ({ children, padding, bg, text, border }: ButtonProps) => {
   return (
@@ -7,7 +7,7 @@ const Button = ({ children, padding, bg, text, border }: ButtonProps) => {
       ${padding ? padding : 'px-10 py-2'}
       ${bg ? bg : 'bg-primary'}
       ${text ? text : 'text-light'}
-      ${border ? `border-2 ${border}` : ''}
+      ${border ? `border-2 ${border}` : 'border-2 border-primary'}
      rounded  text-2xl duration-300 active:translate-y-2`}
     >
       {children}
