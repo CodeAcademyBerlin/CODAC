0fa4f3a007e55c5a5ae07267d3c2ac4a1302eaf7
santiagomanso <santiagomanso@web.de>
2023-03-30 14:35:17 +0200

    ⚗️ testing flex

--- apps/react-tailwind-components/src/components/containers/MainContainer.tsx
+++ apps/react-tailwind-components/src/components/containers/MainContainer.tsx
@@ -2,7 +2,7 @@ import type { typeChildren } from '../../types/types'
 
 const MainContainer = ({ children }: typeChildren) => {
   return (
-    <section className='flex h-screen items-center justify-center bg-gradient-to-br from-gray-700 to-slate-900'>
+    <section className='flex h-screen  flex-row items-center justify-center bg-gradient-to-br from-gray-700 to-slate-900'>
       {children}
     </section>
   )
