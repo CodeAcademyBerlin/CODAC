3403de0028033059dc05bc011be5c0f8e2f2ee9d
santiagomanso <santiagomanso@web.de>
2023-04-03 16:21:59 +0200

    🎨 🔧 reworked css background

--- apps/react-tailwind-components/src/components/containers/MainContainer.tsx
+++ apps/react-tailwind-components/src/components/containers/MainContainer.tsx
@@ -2,7 +2,7 @@ import type { typeChildren } from '../../types/types'
 
 const MainContainer = ({ children }: typeChildren) => {
   return (
-    <section className='flex h-screen  flex-row items-center justify-center bg-gradient-to-br from-gray-700 to-slate-900'>
+    <section className='flex h-screen  flex-row items-center justify-center bg-gradient-to-br from-violet-200 to-fuchsia-300'>
       {children}
     </section>
   )
