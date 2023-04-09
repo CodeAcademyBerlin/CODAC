e95f98577a225a6cf0d7c071119586198b03f1db
atakee72 <atakee@gmail.com>
2023-04-09 03:28:52 +0200

    Created Layout, applied it, prepared for the SSR

---
+++ apps/codac-ts-next-lms/src/components/Layout.tsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+
+function openClose() {
+  const sidebar = document.querySelector("#sidebar");
+  const path = document.querySelector("path");
+
+  if (sidebar?.classList.contains("hidden")) {
+    sidebar.classList.remove("hidden");
+    path?.setAttribute("d", "M6 18L18 6M6 6l12 12");
+  } else {
+    sidebar?.classList.add("hidden");
+    path?.setAttribute("d", "M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75");
+  }
+}
+
+function Navbar() {
+  return (
+    <div className="navbar ml-12 fixed top-1 z-10 flex w-9/12 items-center justify-end gap-1 bg-[#009688] py-3 px-3 text-sm shadow-lg">
+      <div className="mr-auto flex text-md">
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          width={35}
+          onClick={openClose}
+        >
+          <path
+            d="M6 18L18 6M6 6l12 12"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+        </svg>
+        &emsp; &emsp;
+        <p>CodeAcademy Logo</p>
+      </div>
+      <div className="flex gap-2">
+        <div>Links</div>
+        <div>Mode</div>
+        <div>User</div>
+      </div>
+    </div>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div
+      className="sidebar fixed top-1 ml-1 min-h-screen w-2/12 bg-zinc-800 pt-0"
+      id="sidebar"
+    >
+      <div className="sidebar-item">Web Development</div>
+      <div className="sidebar-item">Hello World!</div>
+      <div className="sidebar-item">Courses</div>
+      <div className="sidebar-item">Mentors</div>
+      <div className="sidebar-item">Calendar</div>
+      <div className="sidebar-item">Family</div>
+    </div>
+  );
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="container w-screen">
+      <div className="flex items-start">
+        <div>
+          <Sidebar />
+        </div>
+        <div className="comeover mx-[22%]">
+          <Navbar />
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/codac-ts-next-lms/src/pages/courses/index.tsx
@@ -0,0 +1,70 @@
+import { useGetAllCoursesQuery } from "codac-administration";
+import Layout from "../../components/Layout";
+
+export default function Courses() {
+  const { data, loading, error } = useGetAllCoursesQuery();
+
+  const courses = data?.courses?.data;
+  console.log("🚀 ~ Courses ~ data:", courses);
+
+  return (
+    <Layout>
+      <div className="flex flex-col overflow-y-auto px-1 py-7 ">
+        <h4 className=" my-2 bg-zinc-800 p-3 font-bold text-[#009688] shadow-xl">
+          Courses
+        </h4>
+        <div>
+          SOmething SOmething SOmething SOmething SOmething SOmething SOmething
+          SOmething SOmethingSOmething SOmething SOmething SOmething SOmething
+          SOmething SOmethingSOmething SOmething SOmething SOmething SOmething
+          SOmething SOmething SOmething SOmething SOmething SOmething SOmething
+          SOmething SOmething SOmethingSOmething SOmethingSOmething
+          {!courses
+            ? loading
+            : courses.map((course, i) => {
+                return (
+                  <div
+                    className="my-2 w-fit bg-zinc-800 p-3 text-[#009688] shadow-xl"
+                    key={i}
+                  >
+                    <p>
+                      <strong>Name of the course:</strong> &emsp;{" "}
+                      {course?.attributes?.name}
+                    </p>
+                    <p>
+                      <strong>Description:</strong>
+                      &emsp;
+                      {course?.attributes?.description}
+                    </p>
+                    <div className="flex flex-col">
+                      <div className="flex">
+                        <strong>Mentor 1:</strong> &emsp;
+                        <span>
+                          {
+                            course?.attributes?.mentors.data[0].attributes.user
+                              .data.attributes.firstname
+                          }
+                        </span>
+                      </div>
+                      <div className="flex">
+                        <strong>Mentor 2:</strong> &emsp;
+                        <span>
+                          {
+                            course?.attributes?.mentors.data[1].attributes.user
+                              .data.attributes.firstname
+                          }
+                        </span>
+                      </div>
+                    </div>
+                    <p>
+                      <strong>Period:</strong> &emsp;
+                      {course?.attributes.length} months
+                    </p>
+                  </div>
+                );
+              })}
+        </div>
+      </div>
+    </Layout>
+  );
+}
