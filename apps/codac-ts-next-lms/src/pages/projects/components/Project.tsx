a6fe3a6d87bf66acc156ad10badc150bb21e67a1
Monique Santos <monique@blackmagicbox.com.br>
2023-05-04 13:53:30 +0200

    Feat Sidebar - links adjustments

--- apps/codac-ts-next-lms/src/pages/projects/components/Project.tsx
+++ apps/codac-ts-next-lms/src/pages/projects/components/Project.tsx
@@ -2,45 +2,62 @@ import Link from "next/link";
 import React from "react";
 
 interface Project {
-    id: number | undefined | null
-    attributes: {
-        name: string
-        description: string
-        spikes: any
-    } | undefined | null;
+  id: number | undefined | null;
+  attributes:
+    | {
+        name: string;
+        description: string;
+        spikes: any;
+      }
+    | undefined
+    | null;
 }
 
+const Project = ({
+  projects,
+  projectId,
+}: {
+  projects: Project[];
+  projectId: number;
+}) => {
+  // @ts-ignore
+  const project: Project = Array.isArray(projects)
+    ? projects.find((_project) => _project?.id === projectId)
+    : ({} as Project);
 
-const Project = ({projects, projectId}: { projects: Project[], projectId: number }) => {
-    // @ts-ignore
-    const project: Project = Array.isArray(projects) ?
-        projects.find((_project) => (_project?.id === projectId)) : {} as Project
-
-    return project && Object.keys(project).length > 0 && (
-        <div className="flex justify-center w-full">
-            <a
-                href="#"
-                className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-            >
-                <h2 className="ui-text-xl font-extrabold dark:text-white">
-                    {project?.attributes?.name}
-                </h2>
-                <p className="my-4 text-gray-500">
-                    {project?.attributes?.description}
-                </p>
-                <h3 className="mb-2 text-2xl font-semibold text-gray-900 dark:text-white">Spikes:</h3>
-                <ul className="max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
-                    {project?.attributes?.spikes?.data?.map(({attributes, id}) => (
-                        <li key={id} >
-                            <Link className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href={`spikes/${id}`}>
-                                <span>{attributes?.title}</span>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </a>
-        </div>
-    );
+  return (
+    project &&
+    Object.keys(project).length > 0 && (
+      <div className="flex w-full justify-center">
+        <a
+          href=""
+          className="block rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        >
+          <h2 className="ui-text-xl font-extrabold dark:text-white">
+            {project?.attributes?.name}
+          </h2>
+          <p className="my-4 text-gray-500">
+            {project?.attributes?.description}
+          </p>
+          <h3 className="text-2xl mb-2 font-semibold text-gray-900 dark:text-white">
+            Spikes:
+          </h3>
+          <ul className="max-w-md list-inside list-none space-y-1 text-gray-500 dark:text-gray-400">
+            {project?.attributes?.spikes?.data?.map(({ attributes, id }) => (
+              <li key={id}>
+                <Link
+                  className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  href={`spikes/${id}`}
+                >
+                  <span>{attributes?.title}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </a>
+      </div>
+    )
+  );
 };
 
 export default Project;
--- apps/codac-ts-next-lms/src/pages/spikes/[id].tsx
+++ apps/codac-ts-next-lms/src/pages/spikes/[id].tsx
@@ -1,43 +1,42 @@
-import Link from "next/link";
-import {useRouter} from "next/router";
-import {useGetSpikeQuery} from "codac-administration";
+import { useRouter } from "next/router";
+import { useGetSpikeQuery } from "codac-administration";
 import Header from "../projects/components/Header";
-import Sidebar from "../projects/components/Sidebar";
-import Project from "../projects/components/Project";
-
 
 export default function Projects() {
-    const PAGE_TITLE = 'Spike';
-    const router = useRouter()
-    const {id} = router.query
-    const {data} = useGetSpikeQuery({
-        variables: {
-            id
-        },
-    });
+  const PAGE_TITLE = "Spike";
+  const router = useRouter();
+  const { id } = router.query;
+  const { data } = useGetSpikeQuery({
+    variables: {
+      id,
+    },
+  });
 
-    console.log(data)
-    console.log("spike", data?.spike);
-    return (
-        <div className="container">
-            <Header title={PAGE_TITLE}/>
-            <main className="flex w-screen justify-center items-center pt-8">
-                <a
-                    href="#"
-                    className="block max-w-3xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-                >
-                    <h2 className="ui-text-xl font-extrabold dark:text-white">
-                        {data?.spike?.data?.attributes?.title}
-                    </h2>
-                    <p className="my-4 text-gray-500">Start developing with an open-source library of over 450+
-                        UI components, sections, and pages built with the utility classes from Tailwind CSS and designed
-                        in Figma.</p>
-                    <p className="mb-4 font-normal text-gray-500 dark:text-gray-400">Deliver great service
-                        experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical
-                        development work, eliminate toil, and deploy changes with ease.</p>
-
-                </a>
-            </main>
-        </div>
-    )
+  console.log(data);
+  console.log("spike", data?.spike);
+  return (
+    <div className="container">
+      <Header title={PAGE_TITLE} />
+      <main className="flex w-screen items-center justify-center pt-8">
+        <a
+          href="#"
+          className="block max-w-3xl rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        >
+          <h2 className="ui-text-xl font-extrabold dark:text-white">
+            {data?.spike?.data?.attributes?.title}
+          </h2>
+          <p className="my-4 text-gray-500">
+            Start developing with an open-source library of over 450+ UI
+            components, sections, and pages built with the utility classes from
+            Tailwind CSS and designed in Figma.
+          </p>
+          <p className="mb-4 font-normal text-gray-500 dark:text-gray-400">
+            Deliver great service experiences fast - without the complexity of
+            traditional ITSM solutions. Accelerate critical development work,
+            eliminate toil, and deploy changes with ease.
+          </p>
+        </a>
+      </main>
+    </div>
+  );
 }
