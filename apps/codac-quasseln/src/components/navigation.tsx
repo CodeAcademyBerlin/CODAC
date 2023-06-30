dcca00e534ab920159fbf5223f305c4b367f2d2d
ROXBOZ <hello@roxanne-borloz.net>
2023-06-30 17:01:42 +0200

    fixed navigation to main chat

--- apps/codac-quasseln/src/components/navigation.tsx
+++ apps/codac-quasseln/src/components/navigation.tsx
@@ -5,18 +5,22 @@ import { navigation } from "#/constants/navigation";
 
 const Navigation = () => {
   return (
-    <div className="space-y-10 text-white">
-      {navigation.map((section) => {
+    <div className="go-to-chat-container">
+      <Link className="go-to-chat" href="/main-chat">
+        <svg className="icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+          <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+        </svg>
+        <span>Go to main chat</span>
+      </Link>
+      {/* FOLLOWING NAVIGATION TOTALLY REDUNDANT AND UNNECESSARY ??? */}
+      {/* {navigation.map((section) => {
         return (
           <div key={section.name} className="space-y-5">
             <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
               {section.name}
             </div>
 
-            <div
-              style={{ border: "5px solid green" }}
-              className="grid grid-cols-1 gap-5  lg:grid-cols-2"
-            >
+            <div className="grid grid-cols-1 gap-5  lg:grid-cols-2">
               {section.items.map((item) => {
                 return (
                   <Link
@@ -39,7 +43,7 @@ const Navigation = () => {
             </div>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 };
