0f492ad737615852564948971af018f266f493cf
Lucas Dupias <lucas@codeacademyberlin.com>
2023-07-13 15:31:51 +0200

    fix build

--- apps/codac-community/src/components/community/StudentList.tsx
+++ apps/codac-community/src/components/community/StudentList.tsx
@@ -2,15 +2,14 @@ import { SkeletonCards } from "codac-ui";
 
 import { useGetStudentsByCohorts } from "#/graphql/hooks";
 
-import { StudentCard } from "./studentCard";
+import { StudentCard } from "./StudentCard";
 
 function StudentList({ cohortName }: { cohortName: string }) {
   const { students, loading } = useGetStudentsByCohorts(cohortName);
-  console.log("we wanna see this one, i love console.log ",students);
-  
+  console.log("we wanna see this one, i love console.log ", students);
+
   return (
     <>
- 
       <div className="flex justify-center">
         {loading && <SkeletonCards number={3} isLoading={loading} />}
         <div className="flex grid-cols-5 flex-wrap justify-center">
